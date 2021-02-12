7937dd88d4d601f436000f65afa6778ab0620e43
LaKeisha <lakeisharprice@gmail.com>
2021-02-11 23:09:43 -0500

    updates

---
+++ app1.js
@@ -0,0 +1,20 @@
+
+console.log("Hello world!");
+console.warn("Stop before you go too far!");
+console.error("Error!");
+var myVariable;
+myVariable = "What's up world";
+console.log(myVariable);
+
+var myVariable2 = 123;
+console.log(myVariable+myVariable2);//I believe it will combine the two as one is a text and the other a number.
+/* This is my example of 
+a multi-line comment.  There are three types: //, <!, */
+console.log(typeof myVariable);//I believe it will show the string text
+//expected output: "string"
+var myVariable3 = Text
+console.log(myVariable3);
+myVariable=5;
+myVariable2 = 5;
+console.log(myVariable + myVariable2);
+
